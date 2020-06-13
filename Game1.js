7b3ad80589feac8af3db3e262333d77165920d09
Daniar <dan.zh.1999@gmail.com>
2020-06-13 18:44:52 +0600

    2 Games before Add to one page with css

--- Game1.js
+++ Game1.js
@@ -1,27 +1,30 @@
 var player;
+var playerSize = 30;
 var walls = [];
 var speed = 4;
+var myScore;
 
 function startGame(){
-    player = new component(30, 30, "red", myGameArea.canvas.width/2, myGameArea.canvas.height-30);
+    player = new component(playerSize, playerSize, "red", myGameArea.canvas.width/2, myGameArea.canvas.height-30);
+    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myGameArea.start(); 
 }
 
 
 var myGameArea = {
-    canvas: document.getElementById("myCanvas"),
-    start : function(){
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 320;
+        this.canvas.style.cursor = "none";
         this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;          
-        window.addEventListener('keydown', function (e) {
-            myGameArea.key = e.keyCode;
-        })
-        window.addEventListener('keyup', function (e) {
-            myGameArea.key = false;
-        })
         this.interval = setInterval(updateGameArea, 20);
-
-             
+        window.addEventListener('mousemove', function (e) {
+                    myGameArea.x = e.pageX;
+                    myGameArea.y = e.pageY;
+        })
     },
     clear: function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -31,7 +34,8 @@ var myGameArea = {
     }
 }
 
-function component(width, height, color, x, y){
+function component(width, height, color, x, y, type){
+    this.type = type;
     this.width = width;
     this.height = height;
     this.x = x;
@@ -40,8 +44,14 @@ function component(width, height, color, x, y){
     this.speedY = 0;
     this.update = function(){
         ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     this.newPos = function() {
         this.x += this.speedX;
@@ -69,19 +79,19 @@ function component(width, height, color, x, y){
 
 function updateGameArea(){
     var x, y;
-  for (i = 0; i < walls.length; i += 1) {
-    if (player.crashWith(walls[i])) {
-      myGameArea.stop();
-      return;
+    for (i = 0; i < walls.length; i += 1) {
+        if (player.crashWith(walls[i])) {
+            myGameArea.stop();
+            return;
+        }
     }
-  }
     myGameArea.clear();
     myGameArea.frameNo += 1;
     if (myGameArea.frameNo == 1 || everyinterval(30)) {
         x = myGameArea.canvas.width;
         y = 0;
         width = myGameArea.canvas.width/3;
-        x = width * Math.floor(Math.floor(Math.random()*10)/3);
+        x = width * Math.round(Math.round(Math.random()*10)/3);
         walls.push(new component(width, 10, "green", x, y));
     }
     for (i = 0; i < walls.length; i += 1) {
@@ -93,8 +103,12 @@ function updateGameArea(){
     }
 
     player.speedX = 0;
-    if (myGameArea.key && myGameArea.key == 37) {player.speedX = -12; }
-    if (myGameArea.key && myGameArea.key == 39) {player.speedX = 12; }
+    if (myGameArea.x && myGameArea.y) {
+        player.x = myGameArea.x;
+        player.y = myGameArea.y;
+    }
+    myScore.text = "SCORE: " + myGameArea.frameNo;
+    myScore.update();
     player.newPos();
     player.update();
 }
