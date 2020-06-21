300091ca2c52903a10ee4fd3e7e8cf942b72c530
Daniar <dan.zh.1999@gmail.com>
2020-06-22 00:35:13 +0600

    Adding static folder and env after flask

--- server/runserver.py
+++ server/runserver.py
@@ -1,6 +1,33 @@
 from flask import Flask
+from flask import render_template
+from flask import request, redirect
+from flask import Response
+from flask import send_from_directory
+import os
+
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+
+    return render_template(
+        'index.html',
+
+        )
+
+@app.route('/Game')
+def Game():
+    return render_template(
+        'Game.html'
+        )
+
+@app.route('/ScoreBoard')
+def ScoreBoard():
+    return render_template(
+        'ScoreBoard.html'
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
